931d5a88dd0598fc38ce478af0a6eec6b453cfd7
Ujjwal Gautam <39196584+ujm2099@users.noreply.github.com>
2021-04-24 23:08:46 +0530

    Added the state

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
+  Keyboard,
   KeyboardAvoidingView,
   Platform,
   StyleSheet,
@@ -11,6 +12,19 @@ import {
 import Task from './components/Task';
 
 const App = () => {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  };
+
+  const completeTask = index => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.taskWrapper}>
@@ -18,19 +32,26 @@ const App = () => {
 
         <View style={styles.items}>
           {/* This is where the tasks go */}
-          <Task text={'Task 1'} />
-          <Task text={'Task 2'} />
-          <Task text={'Task 3'} />
-          <Task text={'Task 4'} />
-          <Task text={'Task 5'} />
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
         </View>
       </View>
       {/* Write a task*/}
       <KeyboardAvoidingView
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
         style={styles.writeTaskWrapper}>
-        <TextInput style={styles.input} placeholder={'Write a task'} />
-        <TouchableOpacity>
+        <TextInput
+          style={styles.input}
+          placeholder={'Write a task'}
+          value={task}
+          onChangeText={text => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
           </View>
